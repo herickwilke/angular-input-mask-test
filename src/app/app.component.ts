d33584636cae5ef10c2c4c54dae14d06829550da
Hérick Wilke <herickcwilke@gmail.com>
2023-01-23 10:14:03 -0300

    Created a valid form with a valid mask using the librarie.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { createMask } from '@ngneat/input-mask';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-13';
+
+  // Can be set as a configuration or by binding the user locale in the future
+  localeOptions = {
+    america: {
+      decimalSeparator: '.',
+      thousandSeparator: ',',
+    },
+    europe: {
+      decimalSeparator: ',',
+      thousandSeparator: '.',
+    },
+  };
+
+  testInputMask = createMask({
+    // min
+    // max
+    alias: 'numeric',
+    digits: 2,
+    groupSeparator: this.localeOptions.america.thousandSeparator,
+    radixPoint: this.localeOptions.america.decimalSeparator,
+    digitsOptional: false,
+    allowMinus: true,
+    parser: (value: string) => {
+      return value.replace(',', '');
+    },
+  });
+
+  control = new FormControl();
+
+  ngOnInit(): void {
+    this.control.setValue(1.8);
+  }
+
+  logToConsoleAndReassignValue(value: Number): void {
+    this.control.setValue(value);
+    console.log(this.control);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { InputMaskModule } from '@ngneat/input-mask';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    InputMaskModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
